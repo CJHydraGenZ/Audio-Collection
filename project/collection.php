086a62ad2fe7831c56539fdb50395860552e3824
CJHydraGenZ <cahyasaga321@gmail.com>
2019-06-14 22:37:42 +0800

    update canvas dan play music

--- project/collection.php
+++ project/collection.php
@@ -44,26 +44,7 @@ $music = query("SELECT * FROM tb_music");
             <div class="col-md-2 Pkartu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odio mollitia quae necessitatibus explicabo tempore ipsum laudantium dolorem nostrum, praesentium nobis accusantium fuga! Hic quod iste quo iure. Saepe, libero?</div>
             <div class="col-lg-10 kartu">
                 <div class="box sort"></div>
-                <!-- <div class="box a">
-                    <img src="assets/img/50099555_p0.jpg  " alt="">
-                    <a href="" class="music">laaaaa</a>
-                </div>
-                <div class="box b">
-                    <img src="assets/img/50099555_p0.jpg  " alt="">
-                    <a href="" class="music">laaaaa</a>
-                </div>
-                <div class="box c">
-                    <img src="assets/img/50099555_p0.jpg  " alt="">
-                    <a href="" class="music">laaaaa</a>
-                </div>
-                <div class="box d">
-                    <img src="assets/img/50099555_p0.jpg  " alt="">
-                    <a href="" class="music">laaaaa</a>
-                </div>
-                <div class="box e">
-                    <img src=>assets/img/50099555_p0.jpg  " alt="">
-                    <a href="" class="music">laaaaa</a>
-                </div> -->
+
                 <?php foreach ($music as $lagu) : ?>
 
 
@@ -71,189 +52,19 @@ $music = query("SELECT * FROM tb_music");
                     <div class="card box a">
                         <img src="<?= baseUrl;   ?>assets/img/<?= $lagu['thumbnail'];   ?>      " class="card-img-top" alt="...">
                         <div class="card-body">
-                            <a href=""><?= $lagu['judul'];   ?> </a>
+                            <a href="<?= baseUrl;   ?>project/play.php?id=<?= $lagu['id'];   ?>  "><?= $lagu['judul'];   ?> </a>
                             <p><?= $lagu['deskripsi'];   ?> </p>
                             <div class="row content">
                                 <div class="col artis"><?= $lagu['artis'];   ?> </div>
-                                <div class="col view">3123</div>
-                                <div class="col commen">d</div>
+                                <div class="col like">3123</div>
+                                <div class="col views">d</div>
 
                             </div>
                         </div>
                     </div>
                 <?php endforeach ?>
 
-                <!-- <div class="card box b">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box c">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box d">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box e">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
 
-
-
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg  " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg  " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card box">
-                    <img src="<?= baseUrl;   ?>assets/img/50099555_p0.jpg " class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="">lerem</a>
-                        <div class="row content">
-                            <div class="col icon">a</div>
-                            <div class="col view">3123</div>
-                            <div class="col commen">d</div>
-                            <div class="col pesan">r</div>
-                        </div>
-                    </div>
-                </div> -->
             </div>
         </div>
     </div>
